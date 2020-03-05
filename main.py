ed25a63dd438f2c85c0e87ead8c0cd03e3810f63
Hosea Gibson <hosea.gibs@outlook.com>
2020-03-05 12:36:18 -0800

    added code to take input

--- main.py
+++ main.py
@@ -2,9 +2,21 @@
 import budget_calc
 # Declare Variables
 # Get user input for monthly cash
+monthly = int(input("How much is your monthly cash flow" ))
+
 # Do calculation for 50%
+total = budget_calc.budget_50(monthly)
+
+print(total)
 # Do calculation for 30%
+total2 = budget_calc.budget_30(total)
+
+print(total2)
 # Do calculation for 20%
+
+total3 = budget_calc.budget_20(total2)
+
+print(total3)
 # Ask user if they have a goal and how much they want to put from 20% savings
 # Print to console
 # Save to a pdf file for all information
