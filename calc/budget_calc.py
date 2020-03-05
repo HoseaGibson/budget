81a4e7a310c6fd2a4f1e85b2909c1edc12bea9bc
Hosea Gibson <hosea.gibs@outlook.com>
2020-03-05 10:00:35 -0800

    Create budget calculations

--- calc/budget_calc.py
+++ calc/budget_calc.py
@@ -1,3 +1,8 @@
 # Calculate 50% of cash to necessities
+def budget_50(monthly):
+    percent = .50
+    total = monthly * percent
+    result = monthly - total
+    return result
 # Calculate 30& of cash to wants
 # Calculate 20% of cash to savings
