a997c3f4b4d39fb510175a70f976aaef988543c4
Hosea Gibson <hosea.gibs@outlook.com>
2020-03-05 09:36:47 -0800

    Initial Commit

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+# Declare Variables
+# Get user input for monthly cash
+# Do calculation for 50%
+# Do calculation for 30%
+# Do calculation for 20%
+# Ask user if they have a goal and how much they want to put from 20% savings
+# Print to console
+# Save to a pdf file for all information
---
+++ venv/budget_calc.py
@@ -0,0 +1,3 @@
+# Calculate 50% of cash to neccessities
+# Calculate 30& of cash to wants
+# Calculate 20% of cash to savings
